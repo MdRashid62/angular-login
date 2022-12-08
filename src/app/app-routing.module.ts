f3fce63d428bc9451f9c4ed75ccf4b4104af94f7
MdRashid62 <rashy.mr62@gmail.com>
2022-12-08 20:09:48 +0500

    git init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { MovieComponent } from './movie/movie.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' }, /* This is an empty path when there is no path */
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'movie', component: MovieComponent },
+  { path: '**', component: LoginComponent } /* when none of the path matches */
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  auth: any;
+
+  constructor(private router: Router) { }
+  login(uname: string, pword: string) {
+    if (uname === 'rashid' && pword === '1234') {
+      return 200;
+    }
+    else {
+      return 403;
+    }
+  }
+
+  logout () {
+    this.router.navigate(['login']);
+    }
+}
